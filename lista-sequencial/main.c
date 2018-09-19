934280e0a062cbe8f1331ceee8fa3ec112b6215e
Francisco <hider1195@gmail.com>
2018-09-19 11:28:33 -0300

    Adicionada as funções buscar e deletar na lista sequencial

--- lista-sequencial/main.c
+++ lista-sequencial/main.c
@@ -19,11 +19,35 @@ LISTA_SEQ criarLista(int tamanho) {
 }
 
 bool inserir(LISTA_SEQ *lista, int elemento) {
-	if(lista->cardinalidade < lista->tamanho) {
+	if (lista->cardinalidade < lista->tamanho) {
 		(lista->ultimo_elemento) += 1;
 		lista->vetor[lista->ultimo_elemento] = elemento;
 		(lista->cardinalidade) += 1;
 		return true;
+	}
+	return false;
+}
+
+int buscar(LISTA_SEQ *lista, int valor) {
+	for (int i = 0; i < lista->tamanho; ++i) {
+		if (lista->vetor[i] == valor) {
+			return i;
+			break;
+		}
+	}
+	return -1;
+}
+
+bool deletar(LISTA_SEQ *lista, int valor) {
+	int indice = buscar(lista, valor);
+	if (indice != -1) {
+		if ((lista->cardinalidade) >= 0) {
+			for (int i = indice; i < (lista->cardinalidade) - 1; ++i) {
+				lista->vetor[i] = lista->vetor[i + 1];
+			}
+			(lista->cardinalidade) -= 1;
+			return true;
+		}
 	}
 	return false;
 }
@@ -33,14 +57,14 @@ int main(int argc, char const *argv[]) {
 	LISTA_SEQ lista;
 	int size = 5;
 	bool result = true;
-	int valor;
+	int elemento, valor;
 
 	lista = criarLista(size);
 
 	printf("Digite os valores para inserir na lista:\n");
 	for (int i = 0; i < size; ++i) {
-		scanf("%i", &valor);
-		if(!inserir(&lista, valor)) break;
+		scanf("%i", &elemento);
+		if(!inserir(&lista, elemento)) break;
 	}
 
 	printf("\n");
@@ -49,6 +73,20 @@ int main(int argc, char const *argv[]) {
 		printf("%i\n", lista.vetor[i]);
 	}
 
+	printf("\n");
+	printf("Digite o valor a ser procurado na lista: ");
+	scanf("%i", &valor);
+	if(buscar(&lista,valor) != -1) printf("%i\n", buscar(&lista, valor)); 
+
+	if(deletar(&lista, valor)) printf("Elemento deletado\n");
+	else printf("Elemento não foi deletado\n");
+
+	if(deletar(&lista, valor + 1)) printf("Elemento deletado\n");
+	else printf("Elemento não foi deletado\n");
+
+	for (int i = 0; i < lista.cardinalidade; ++i) {
+		printf("%i\n", lista.vetor[i]);
+	}
 
 	return 0;
 }
